6e90726477cae42da8e18319147f63ca698d5782
sunxyw <git@sunxyw.xyz>
2022-10-16 21:32:57 +0800

    enhance providers test

--- tests/ProvidersTest.php
+++ tests/ProvidersTest.php
@@ -3,9 +3,10 @@
 test('all providers can shorten', function () {
     $providers = \Illuminate\Support\Facades\Config::get('unify-shorten.providers');
     unset($providers['default']);
-    $providers = array_keys($providers);
-    foreach ($providers as $provider) {
-        $url = \Sunxyw\Shorten\Facades\Shorten::provider($provider)->shorten('https://google.com');
+    $shorten = app(\Sunxyw\Shorten\Shorten::class);
+    foreach ($providers as $provider => $config) {
+        $url = $shorten->provider($provider)->shorten('https://google.com');
         expect($url)->toBeString();
+        expect($url)->toStartWith($config['options']['base_url']);
     }
 });
