f1ae25dad1ce3a4e40470747620028bb68aef6fe
sunxyw <sunxyw@users.noreply.github.com>
2022-11-16 02:46:59 +0000

    Fix styling

--- src/Providers/Firebase.php
+++ src/Providers/Firebase.php
@@ -9,7 +9,7 @@ class Firebase extends Provider
 {
     public function shorten(string $url, string $code = null): string
     {
-        $response = Http::post($this->api . '?key=' . $this->option('api_key'), [
+        $response = Http::post($this->api.'?key='.$this->option('api_key'), [
             'dynamicLinkInfo' => [
                 'domainUriPrefix' => rtrim($this->option('base_url'), '/'),
                 'link' => $url,
